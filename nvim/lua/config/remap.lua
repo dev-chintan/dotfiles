84a7de63586ef11cf01a6d9dbc3ef9fb13c419e2
dev-chintan <chintanpatel1226@gmail.com>
2024-10-30 10:29:10 -0500

    added compile and execute for cpp files

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<leader>gu", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>gb", "<C-^>")
+
+vim.api.nvim_set_keymap("n", "<leader>cr", ":w<CR>:!g++ % -o %:r && ./%:r<CR>", { noremap = true, silent = true })
+
