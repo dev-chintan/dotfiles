f6b01c1f13c6ccc321a6516d364c535273d6209f
dev-chintan <chintanpatel1226@gmail.com>
2024-12-22 08:46:06 -0600

    update

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<leader>gu", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>gb", "<C-^>")
+vim.keymap.set('n', '<leader>', ':w<CR>:!g++ -std=c++17 % -o %:r && ./%:r<CR>', {noremap = true, silent = false})
+
+
