cc654982781e6e420d410a242f177d33b35ab094
dev-chintan <chintanpatel1226@gmail.com>
2024-12-22 08:47:13 -0600

    remove remap

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,5 +1,2 @@
 vim.keymap.set("n", "<leader>gu", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>gb", "<C-^>")
-vim.keymap.set('n', '<leader>', ':w<CR>:!g++ -std=c++17 % -o %:r && ./%:r<CR>', {noremap = true, silent = false})
-
-
